02a2b291debc73d0454c8cdefadf82e50b0faed4
renato-av <renato.avela.17@gmail.com>
2023-10-16 16:33:35 -0500

    <fix>: misspelling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className='footer'>
       <h4>Projects <span>@renato_av</span></h4>
-      <h5>Shopping Cart con useContext & useReducer</h5>
+      <h5>Shopping Cart with useContext & useReducer</h5>
     </footer>
   )
 }
